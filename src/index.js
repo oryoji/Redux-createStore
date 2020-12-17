f0333879087531a04bc08905368c28cf4d7b1b75
oryoji <60314819+oryoji@users.noreply.github.com>
2020-12-17 19:40:47 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "PLUS_ONE":
+      return state + 1;
+
+    case "MINUS_ONE":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+console.log(store);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// store.dispatch({ type: "PLUS_ONE" });
+// store.dispatch({ type: "PLUS_ONE" });
+
+store.dispatch({ type: "MINUS_ONE" });
